1e21709bbf17f66b962471b5d2300084e89ea300
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-03-06 13:28:51 +0300

    Добавляет thankyou

--- build/js/script.js
+++ build/js/script.js
@@ -1,10 +1,13 @@
 'use strict';
 const modalQuestionsTemplate = document.querySelector('#questions').content.querySelector('.questions');
+const thankYouTanplate = document.querySelector('#thankyou').content.querySelector('.thankyou');
 const body = document.querySelector('body');
 const linksToModal = document.querySelectorAll('.header__link');
+const newModal = modalQuestionsTemplate.cloneNode(true);
+const formSubscribe = newModal.querySelector('.form-subscribe')
 
 const onIndexReturn = (evt) => {
-  if (evt.key !== 'Escape' && evt.target.classList.contains('questions__box--m')) {
+  if (evt.key !== 'Escape' || evt.target.classList.contains('questions__box--m')) {
     return;
   }
   evt.preventDefault();
@@ -15,7 +18,6 @@ const onIndexReturn = (evt) => {
 
 const onlinksToModalClick = (evt) => {
   evt.preventDefault();
-  const newModal = modalQuestionsTemplate.cloneNode(true);
   body.insertAdjacentElement('afterbegin', newModal);
   body.classList.add('overflow');
   const returnToIndex = newModal.querySelector('.questions__return-text');
@@ -23,6 +25,14 @@ const onlinksToModalClick = (evt) => {
   document.addEventListener('keydown', onIndexReturn);
 }
 
+const onSubscribe = (evt) => {
+  evt.preventDefault();
+  newModal.remove();
+  const newThankYou = thankYouTanplate.cloneNode(true);
+  body.insertAdjacentElement('afterbegin', newThankYou);
+}
+
 linksToModal.forEach((link) => {
   link.addEventListener('click', onlinksToModalClick)
 });
+formSubscribe.addEventListener('submit', onSubscribe)
